ca7e76adc9c13766840706ce1d182e518e761db5
shadowzlh <shadowzlh@foxmail.com>
2024-10-31 21:38:13 +0800

    search

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
+use i_slint_backend_winit::WinitWindowAccessor;
+
 slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let app = App::new()?;
 
     let app_weak = app.as_weak();
-    app.on_mouse_move(move |delta_x, delta_y| {
+    app.on_mouse_move(move || {
         let app_weak = app_weak.unwrap();
-        let logical_pos = app_weak.window().position().to_logical(app_weak.window().scale_factor());
-        app_weak.window().set_position(slint::LogicalPosition::new(logical_pos.x + delta_x, logical_pos.y + delta_y));
-        // app_weak.drag_window();
         app_weak.window().with_winit_window(|win| {
             let _ = win.drag_window();
         });
     });
+    let app_weak = app.as_weak();
+
+
+    app.on_search(move |text| {
+        println!("{:?}",text);
+    });
+    println!("asd ");
     app.run()
 }
